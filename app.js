7b0d605393b7b43f52fb1d380baf4309cfb9b8a7
Alexey <alexey.mipt@gmail.com>
2020-04-21 13:57:50 +0200

    use public

--- app.js
+++ app.js
@@ -2,28 +2,32 @@ var express=require('express');
 var path = require('path');
 var app=express();
 var bodyParser = require("body-parser");
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+if (process.env["TEST_RESULT_USE_REVERSE_PROXY"] == null) {
+    app.use(express.static(path.join(__dirname,'public')));
+}
 
-app.get('/',function(req,res){
-    res.sendFile("index.html", {root: path.join(__dirname, 'public')});
-});
-app.get('/positive',function(req,res){
-    res.sendFile("positive.html", {root: path.join(__dirname, 'public')});
-});
-app.get('/negative',function(req,res){
-    res.sendFile("negative.html", {root: path.join(__dirname, 'public')});
-});
+// app.get('/',function(req,res){
+//     res.sendFile("index.html", {root: path.join(__dirname, 'public')});
+// });
+// app.get('/positive',function(req,res){
+//     res.sendFile("positive.html", {root: path.join(__dirname, 'public')});
+// });
+// app.get('/negative',function(req,res){
+//     res.sendFile("negative.html", {root: path.join(__dirname, 'public')});
+// });
 
 app.post('/query-result',function(req,res)
         {
             if (req.body.testid === '+') {
-                res.redirect('/positive');
+                res.redirect('/positive.html');
             } else {
-                res.redirect('/negative');
+                res.redirect('/negative.html');
             }
             console.log(req.body);
         });
 PORT = process.env["TEST_RESULT_PORT"] || 1234;
 
-var server=app.listen(1234,function() {});
+var server=app.listen(PORT, function() {});
