e68eee5ee1dccaadd36e2c44e7e9e38511f70b50
Alexey <alexey.mipt@gmail.com>
2020-04-21 11:52:41 +0200

    receive post test id

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
 var express=require('express');
+var path = require('path');
 var app=express();
 var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get('/',function(req,res){
-    res.sendfile("index.html");
+    res.sendFile("index.html", {root: path.join(__dirname, 'public')});
+});
+app.get('/positive',function(req,res){
+    res.sendFile("positive.html", {root: path.join(__dirname, 'public')});
+});
+app.get('/negative',function(req,res){
+    res.sendFile("negative.html", {root: path.join(__dirname, 'public')});
 });
 
 app.post('/query-result',function(req,res)
         {
-            res.send('Hello World!');
+            if (req.body.testid === '+') {
+                res.redirect('/positive');
+            } else {
+                res.redirect('/negative');
+            }
             console.log(req.body);
         });
 var server=app.listen(1234,function() {});
