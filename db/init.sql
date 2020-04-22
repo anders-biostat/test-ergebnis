240cae8bb7189e6d792be397e44ff305e27803d9
Alexey <alexey.mipt@gmail.com>
2020-04-22 15:57:52 +0200

    upd tables

--- db/init.sql
+++ db/init.sql
@@ -2,4 +2,5 @@ CREATE TABLE results
 (
   id VARCHAR(255) NOT NULL PRIMARY KEY,
   result VARCHAR(255)
+  -- timestamp DATE DEFAULT (datetime('now','localtime'))
 )
--- test/test-table.js
+++ test/test-table.js
@@ -2,13 +2,13 @@ var sqlite = require("sqlite3");
 
 dbase = new sqlite.Database("db/results.db");
 
-sql = "INSERT INTO results VALUES (?, ?)";
+sql = "INSERT INTO results (id, result) VALUES (?, ?)";
 
 res = [
-    ["res1", "+"],
-    ["res2", "-"],
-    ["res3", "+"],
-    ["res4", "-"],
-    ["res5", "+"],
+    ["positive", "positive"],
+    ["negative", "negative"],
+    ["inconclusive", "inconclusive"],
+    ["empty", ""],
+    ["res5", "positive"],
 ];
 res.forEach((x) => dbase.run(sql, x));
