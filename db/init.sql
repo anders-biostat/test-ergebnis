494106374229a9f5da0065408dc4fa6a4a658bd9
Alexey <alexey.mipt@gmail.com>
2020-04-22 17:42:28 +0200

    add timestamp

--- db/init.sql
+++ db/init.sql
@@ -1,6 +1,6 @@
 CREATE TABLE results
 (
   id VARCHAR(255) NOT NULL PRIMARY KEY,
-  result VARCHAR(255)
-  -- timestamp DATE DEFAULT (datetime('now','localtime'))
+  result VARCHAR(255),
+  timestamp DATE DEFAULT (datetime('now', 'localtime'))
 )
