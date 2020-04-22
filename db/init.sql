b95f761196173977c6c8a0413eadb8f57773629b
tyranchick <svetlana.ovchinnikova@helsinki.fi>
2020-04-22 20:05:48 +0200

    column added to the results database

--- db/init.sql
+++ db/init.sql
@@ -2,5 +2,6 @@ CREATE TABLE results
 (
   id VARCHAR(255) NOT NULL PRIMARY KEY,
   result VARCHAR(255),
-  timestamp DATE DEFAULT (datetime('now', 'localtime'))
+  timestamp DATE DEFAULT (datetime('now', 'localtime')),
+  seen TINYINT(1) DEFAULT 0
 )
