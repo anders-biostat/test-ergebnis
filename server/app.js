eb6fc14cce65ca57a65ceb94256765d7b2d4f77e
Alexey <alexey.mipt@gmail.com>
2020-04-23 11:26:44 +0200

    add logging

--- server/app.js
+++ server/app.js
@@ -5,6 +5,15 @@ var bodyParser = require("body-parser");
 var db = require("./db.js");
 var throttle = require("express-throttle");
 
+const fs = require('fs');
+var accesslog = null;
+if (process.env["TEST_RESULT_LOG_FILE"] == null) {
+    accesslog = 'log/access.log';
+} else {
+    accesslog = process.env["TEST_RESULT_LOG_FILE"];
+}
+
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 if (process.env["TEST_RESULT_USE_REVERSE_PROXY"] == null) {
@@ -25,6 +34,8 @@ if (process.env["TEST_RESULT_THROTTLE_RATE"] == null) {
 app.post('/query-result', throttle(throttleOptions), async function(req,res)
          {
              testid = req.body.testid;
+             logmsg = testid + '\t' + new Date().toISOString() +'\n';
+             fs.appendFile(accesslog, logmsg, (err) => {if (err) throw err;});
 
              try {
              const result = await db.getTestResult(testid);
